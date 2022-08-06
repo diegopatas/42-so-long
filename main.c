072240bcb79ad47c953c4acc21ec6504bbd3ca60
ddiniz <ddiniz@student.42sp.org.br>
2022-08-06 16:37:47 -0300

    Update project.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:54:38 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/08/03 12:00:50 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/08/05 18:26:41 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int main(void)
 	void	*window;
 
 	connection = mlx_init();
-	window = mlx_new_window(connection, 20, 20, "BOMBER MAN");
+	window = mlx_new_window(connection, 100, 100, "BOMBER MAN");
+	mlx_loop(connection);
 	mlx_destroy_window(connection, window);
 	return (0);
 }
