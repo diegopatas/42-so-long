d0624f9436f686cc609e4037e298f248f3a8302a
ddiniz <ddiniz@student.42sp.org.br>
2022-08-11 22:30:11 -0300

    Add standard headers and a type header.

---
+++ includes/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 21:25:00 by ddiniz            #+#    #+#             */
+/*   Updated: 2022/08/10 17:12:29 by ddiniz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+# include <mlx.h>
+# include <stdlib.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <stdio.h>
+
+# include "types.h"
+
+#endif
