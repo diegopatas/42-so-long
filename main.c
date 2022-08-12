ac916bcb70063b7dec32be576e73dd2f0ebe99a5
ddiniz <ddiniz@student.42sp.org.br>
2022-08-11 22:28:50 -0300

    Implement a full functional loop for the game.

--- main.c
+++ main.c
@@ -6,20 +6,47 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:54:38 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/08/05 18:26:41 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/08/11 20:42:27 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "so_long.h"
 
-int main(void)
+int	render(t_data *data)
 {
-	void	*connection;
-	void	*window;
+	if (data->connect != NULL)
+		return (1);
+	mlx_put_image_to_window(data->connect, data->window, data->img, 0, 0);
+	return (0);
+}
 
-	connection = mlx_init();
-	window = mlx_new_window(connection, 100, 100, "BOMBER MAN");
-	mlx_loop(connection);
-	mlx_destroy_window(connection, window);
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->connect, data->window);
+		mlx_destroy_image(data->connect, data->img);
+	}
+	printf("Key Pressed: %d\n", keysym);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.connect = mlx_init();
+	data.img_width = SPRITE_WIDTH;
+	data.img_height = SPRITE_HEIGHT;
+	data.img = mlx_xpm_file_to_image(data.connect,
+			"./images/ent.xpm", 
+			&data.img_width, 
+			&data.img_height);
+	data.window = mlx_new_window(data.connect, 300, 300, "BOMBER MAN");
+	mlx_loop_hook(data.connect, &render, &data);
+	mlx_hook(data.window, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_loop(data.connect);
+	mlx_destroy_display(data.connect);
+	free(data.connect);
 	return (0);
 }
