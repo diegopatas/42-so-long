7533cc8ffca5cebf7656e3b688388bdfe1f49282
ddiniz <ddiniz@student.42sp.org.br>
2022-08-11 22:31:39 -0300

    Update prototype file related to minilibx.

--- proto.c
+++ proto.c
@@ -1,57 +1,29 @@
-// Window
+// Connection
 	void *mlx_init();
+
+// Window
 	void *mlx_new_window ( void *mlx_ptr, int size_x, int size_y, char *title );
 	int mlx_clear_window ( void *mlx_ptr, void *win_ptr );
 	int mlx_destroy_window ( void *mlx_ptr, void *win_ptr );
 
 // loop
-       int
-       mlx_loop ( void *mlx_ptr );
+       int mlx_loop ( void *mlx_ptr );
 
-       int
-       mlx_key_hook ( void *win_ptr, int (*funct_ptr)(), void *param );
-
-       int
-       mlx_mouse_hook ( void *win_ptr, int (*funct_ptr)(), void *param );
-
-       int
-       mlx_expose_hook ( void *win_ptr, int (*funct_ptr)(), void *param );
-
-       int
-       mlx_loop_hook ( void *mlx_ptr, int (*funct_ptr)(), void *param );
+// Events
+       int mlx_key_hook ( void *win_ptr, int (*funct_ptr)(), void *param );
+       int mlx_mouse_hook ( void *win_ptr, int (*funct_ptr)(), void *param );
+       int mlx_expose_hook ( void *win_ptr, int (*funct_ptr)(), void *param );
+       int mlx_loop_hook ( void *mlx_ptr, int (*funct_ptr)(), void *param );
          expose_hook(void *param);
          key_hook(int keycode,void *param);
          mouse_hook(int button,int x,int y,void *param);
          loop_hook(void *param);
 
 // image
-       void *
-       mlx_new_image ( void *mlx_ptr, int width, int height );
-
-       char *
-       mlx_get_data_addr ( void *img_ptr, int *bits_per_pixel, int *size_line, int *endian );
-
-       int
-       mlx_put_image_to_window ( void *mlx_ptr, void *win_ptr, void *img_ptr, int x, int y );
-
-       unsigned int
-       mlx_get_color_value ( void *mlx_ptr, int color );
-
-       void *
-       mlx_xpm_to_image ( void *mlx_ptr, char **xpm_data, int *width, int *height );
-
-       void *
-       mlx_xpm_file_to_image ( void *mlx_ptr, char *filename, int *width, int *height );
-
-       int
-       mlx_destroy_image ( void *mlx_ptr, void *img_ptr );
-
-// window
-       void *
-       mlx_new_window ( void *mlx_ptr, int size_x, int size_y, char *title );
-
-       int
-       mlx_clear_window ( void *mlx_ptr, void *win_ptr );
-
-       int
-       mlx_destroy_window ( void *mlx_ptr, void *win_ptr );
+       void *mlx_new_image ( void *mlx_ptr, int width, int height );
+//        char *mlx_get_data_addr ( void *img_ptr, int *bits_per_pixel, int *size_line, int *endian );
+       int mlx_put_image_to_window ( void *mlx_ptr, void *win_ptr, void *img_ptr, int x, int y );
+       unsigned int mlx_get_color_value ( void *mlx_ptr, int color );
+       void *mlx_xpm_to_image ( void *mlx_ptr, char **xpm_data, int *width, int *height );
+       void *mlx_xpm_file_to_image ( void *mlx_ptr, char *filename, int *width, int *height );
+       int mlx_destroy_image ( void *mlx_ptr, void *img_ptr );
