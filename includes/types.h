a4e953cbd58a14821bc8cc772f3aa4f938afbefb
ddiniz <ddiniz@student.42sp.org.br>
2022-09-19 17:14:20 -0300

    Update s_game struct.

--- includes/types.h
+++ includes/types.h
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 10:04:26 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/09/12 19:04:04 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/09/16 22:27:23 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 # define SPRITE_HEIGHT 16
 
 typedef struct s_game {
-		void	*connect;
-		void	*window;
+		void	*mlx;
+		void	*win;
 		void	*img;
+// 		char	*sprite = "../assets/sprites/sprite_wall.xpm";
 		int		img_width;
 		int		img_height;
 } t_game;
