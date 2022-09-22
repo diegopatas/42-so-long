5bf7055d604fc176597a1cade7c46e22da43de5f
ddiniz <ddiniz@student.42sp.org.br>
2022-09-21 22:54:53 -0300

    Update main and game_init module.

--- sources/game_init.c
+++ sources/game_init.c
@@ -6,13 +6,17 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 14:14:29 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/09/21 14:15:38 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/09/21 21:51:47 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 
-void	game_init(void)
+int	game_init(t_game *game)
 {
-
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, 300, 300, "win");
+	game->img_width = SPRITE_WIDTH;
+	game->img_height = SPRITE_HEIGHT;
+	return (EXIT_SUCCESS);
 }
