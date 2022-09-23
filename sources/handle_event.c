48d4e25bc31baf8247fbe6955a1eb105082d1f59
ddiniz <ddiniz@student.42sp.org.br>
2022-09-22 23:07:11 -0300

    Refactor render and handle_event modules.

--- sources/handle_event.c
+++ sources/handle_event.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 18:54:09 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/09/16 10:32:08 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/09/22 22:30:28 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	handle_event(int keycode, t_game *game)
 	if (keycode == XK_Escape)
 	{
 // 		mlx_loop_end(game->mlx);
-		mlx_destroy_image(game->mlx, game->img);
+		mlx_destroy_image(game->mlx, game->img_player);
 		mlx_destroy_window(game->mlx, game->win);
 		mlx_destroy_display(game->mlx);
 		free(game->mlx);
--- sources/render.c
+++ sources/render.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 18:58:57 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/09/16 10:32:01 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/09/22 22:53:03 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	render(t_game *game)
 {
-	mlx_put_image_to_window(game->mlx, game->win, game->img, SPRITE_WIDTH, SPRITE_HEIGHT);
+	mlx_put_image_to_window
+		(game->mlx, game->win, game->img_player, SPRITE_WIDTH, SPRITE_HEIGHT);
+	mlx_put_image_to_window
+		(game->mlx, game->win, game->img_wall, 40, 40);
 	return (0);
 }
