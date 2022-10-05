bd292e4e8b7adf97498929eec32d1139d208e5b0
ddiniz <ddiniz@student.42sp.org.br>
2022-10-04 23:15:46 -0300

    Update unit tests.

--- tests/test_main.c
+++ tests/test_main.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:45:58 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/09/28 18:04:51 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/10/03 21:07:32 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 void	run_all_tests(void)
 {
 	RUN_TEST_GROUP(game_init);
-	RUN_TEST_GROUP(sprites_load);
 	RUN_TEST_GROUP(map_valid);
-	RUN_TEST_GROUP(sprites_unload);
 	RUN_TEST_GROUP(map_init);
+	RUN_TEST_GROUP(map_draw);
+	RUN_TEST_GROUP(sprites_load);
+	RUN_TEST_GROUP(sprites_unload);
 }
 
 int	main(int argc, const char *argv[])
