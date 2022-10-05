49cb922e73710cf7fc101e4c0635b8cc591781f3
ddiniz <ddiniz@student.42sp.org.br>
2022-10-04 23:16:43 -0300

    Add map_draw module.

---
+++ sources/map_draw.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_draw.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 16:23:38 by ddiniz            #+#    #+#             */
+/*   Updated: 2022/10/04 23:09:25 by ddiniz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <so_long.h>
+
+void	map_draw_line(t_game *game, int row, int col)
+{
+	void	*sprite;
+
+	sprite = NULL;
+	if (game->map[row][col] == 1)
+		sprite = game->wall;
+	if (game->map[row][col] == 'E')
+		sprite = game->wayout;
+	if (game->map[row][col] == 'C')
+		sprite = game->collect;
+	if (game->map[row][col] == 'P')
+		sprite = game->player;
+	if (game->map[row][col] == 0)
+		sprite = game->empty;
+	mlx_put_image_to_window(game->mlx, game->win, sprite,
+			SPRITE_BASE_WIDTH * row, SPRITE_BASE_HEIGHT * col);
+	return ;
+}
+
+void	map_draw(t_game *game)
+{
+	int	rows;
+	int	cols;
+
+	rows = 0;
+	while (rows < ROWS)
+	{
+		cols = 0;
+		while (cols < COLS)
+		{
+			map_draw_line(game, rows, cols);
+			cols++;
+		}
+		rows++;
+	}
+	return ;
+}
