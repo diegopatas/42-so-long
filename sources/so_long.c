3967cf7e08002d89785d039a1528731130344c5d
ddiniz <ddiniz@student.42sp.org.br>
2022-10-06 12:49:50 -0300

    Refactor main function.

--- sources/so_long.c
+++ sources/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:54:38 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/10/04 15:34:28 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/10/06 12:28:22 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	main(int argc, char *argv[1])
 	game = (t_game *)malloc(sizeof (t_game));
 	if (argc == 2)
 		return (game_run(game, argv[1]));
-	ft_printf("Invalid number of arguments%c!\n", 0);
+	log_message("Invalid number of arguments.");
+	log_message("Please informe a valid map.");
+	log_message("Try './so_long assests/maps/map.ber'");
 	free(game);
 	return (EXIT_SUCCESS);
 }
