47aa8ffbc3e1be525d0fd88c2f924b06584cd76b
ddiniz <ddiniz@student.42sp.org.br>
2022-10-06 12:50:29 -0300

    Add more event handling logic.

--- sources/handle_event.c
+++ sources/handle_event.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 18:54:09 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/10/04 22:46:29 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/10/06 09:47:09 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,27 @@
 
 int	handle_event(int keycode, t_game *game)
 {
-	if (keycode == XK_Escape)
+	if (keycode == XK_Escape || keycode == XK_q || keycode == XK_Q)
+		game_unload(game);
+	else if (keycode == XK_w || keycode == XK_W || keycode == XK_Up)
 	{
-		sprites_unload(game);
-		mlx_destroy_window(game->mlx, game->win);
-		mlx_destroy_display(game->mlx);
-		free(game->mlx);
-		free(game);
-		exit(0);
+		player_position_get(game);
+		player_move_check(game, MOVE_UP, game->p_row, game->p_col);
 	}
-	if (keycode == XK_c)
+	else if (keycode == XK_s || keycode == XK_S || keycode == XK_Down)
 	{
-		if(game->map[1][2] != 'P')
-		{
-			game->map[1][2] = 'P';
-// 			game->map[1][4] = 'C';
-		}
-// 		if(game->map[1][2] == 'P')
-// 		{
-// 			game->map[1][4] = 'P';
-// 			game->map[1][2] = 'C';
-// 		}
-		return (0);
+		player_position_get(game);
+		player_move_check(game, MOVE_DOWN, game->p_row, game->p_col);
 	}
-	if (keycode == XK_d)
+	else if (keycode == XK_a || keycode == XK_A || keycode == XK_Left)
 	{
-		if(game->map[1][2] != 'C')
-			game->map[1][2] = 'C';
-// 		if(game->map[1][2] == 'P')
-// 			game->map[1][2] = 'C';
-		return (0);
+		player_position_get(game);
+		player_move_check(game, MOVE_LEFT, game->p_row, game->p_col);
+	}
+	else if (keycode == XK_d || keycode == XK_D || keycode == XK_Right)
+	{
+		player_position_get(game);
+		player_move_check(game, MOVE_RIGHT, game->p_row, game->p_col);
 	}
 	return (0);
 }
