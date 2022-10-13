487f7b294cc2ecb74af7b953aac5d23af15175aa
ddiniz <ddiniz@student.42sp.org.br>
2022-10-13 19:26:33 -0300

    Fix no file handling.

--- sources/file_is_valid.c
+++ sources/file_is_valid.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 09:23:52 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/10/12 14:30:06 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/10/13 19:21:23 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	file_is_valid(char *file)
 	char	*ext_target;
 
 	ext_target = ft_strrchr(file, '.');
+	if (open(file, O_CLOEXEC) == -1)
+		return (log_message ("map's file can't be open!", ERROR));
 	if (!ext_target)
 		return (log_message("missing extention of type '.ber'!", ERROR));
 	ft_printf("Log: file's extention is '%s'.\n", ext_target);
 	if (ft_strncmp(ext_target, ".ber", 5) == 0)
 		return (log_message("map's file type is valid!", LOG));
-	return (log_message ("map's file is invalid or it doesn't exist!", ERROR));
+	return (log_message ("map's file is invalid!", ERROR));
 }
--- sources/so_long.c
+++ sources/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:54:38 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/10/12 14:36:21 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/10/13 19:16:00 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char *argv[1])
 	if (argc == 2 && file_is_valid(argv[1]))
 		return (game_run(argv[1]));
 	log_message("invalid arguments!", ERROR);
-	log_message("please informe a valid map!", LOG);
+	log_message("please inform a valid map!", LOG);
 	log_message("try './so_long assets/maps/map.ber'", LOG);
 	return (EXIT_SUCCESS);
 }
